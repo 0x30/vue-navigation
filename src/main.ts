2b253a8d9b66062d1fcac9e9c09890c658cf9ff6
jingwenzheng <alphabetion@gmail.com>
2023-04-12 16:38:48 +0800

    chore: 移除例子

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App";
-import { navigation, onPageChange } from "./core";
-
-onPageChange((type, params) => {
-  console.log(type, params);
-});
+import { navigation } from "./core";
 
 createApp(App)
   .use(navigation({ page: "home" }))
