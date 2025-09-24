305bdd810b131f83653895cbf64d3b52cacbf794
0x30 <alphabetion@gmail.com>
2025-09-24 15:06:49 +0800

    feat: 简化开发流程

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import jsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
   plugins: [jsx()],
+  resolve: {
+    alias: {
+      // 直接引用 packages 的源码，而不是编译后的 dist 文件
+      "@0x30/vue-navigation": resolve(fileURLToPath(new URL(".", import.meta.url)), "packages/core/src/index.ts"),
+      "@0x30/vue-navigation-layout": resolve(fileURLToPath(new URL(".", import.meta.url)), "packages/layout/src/index.ts"),
+    },
+  },
+  // 优化依赖预构建
+  optimizeDeps: {
+    include: ["vue"],
+  },
 });
